081e266177389c4001396a75ed2784a080177e52
AngelicaRGomes <angelicarodrigues@id.uuf.br>
2023-05-08 18:25:58 -0300

    adding post method

--- womenController.js
+++ womenController.js
@@ -1,28 +1,50 @@
 
 // const mostraPorta = () => console.log(`Servidor rodando na porta ${port}.`);
+const { v4: uuidv4 } = require('uuid');
+const womenInTI = [
+    {
+        id: "1",
+        name: "Angélica Rodrigues Gomes",
+        image: "https://media.licdn.com/dms/image/D4D03AQHQNm8kW7Agww/profile-displayphoto-shrink_400_400/0/1678667807168?e=1689206400&v=beta&t=MVTyKsxWUfNWeBwE-PAS_RejhE2VmCCcJzvddqnL90s",
+        resume: "Desenvolvedora fullstack"
+    },
+    {
+        id: "2",
+        name: "Lorena Miranda",
+        image: "https://media.licdn.com/dms/image/D4D03AQHQNm8kW7Agww/profile-displayphoto-shrink_400_400/0/1678667807168?e=1689206400&v=beta&t=MVTyKsxWUfNWeBwE-PAS_RejhE2VmCCcJzvddqnL90s",
+        resume: "Desenvolvedora fullstack"
+    },
+    {
+        id: "3",
+        name: "Loiane Groner",
+        image: "https://media.licdn.com/dms/image/D4D03AQHQNm8kW7Agww/profile-displayphoto-shrink_400_400/0/1678667807168?e=1689206400&v=beta&t=MVTyKsxWUfNWeBwE-PAS_RejhE2VmCCcJzvddqnL90s",
+        resume: "Desenvolvedora fullstack"
+    }
+]
+
 
 const showHello = (req, res) => (res.send("Olá, mundo!"));
 
+
+const createWoman = (req, res) => {
+    const createNewWoman = {
+        id: uuidv4(),
+        name: req.body.name,
+        image: req.body.image,
+        resume: req.body.resume
+    }
+
+    womenInTI.push(createNewWoman)
+
+    res.json(womenInTI)
+
+}
+
 const women = (req, res) => {
-    const womenInTI = [
-        {
-            name: "Angélica Rodrigues Gomes",
-            image: "https://media.licdn.com/dms/image/D4D03AQHQNm8kW7Agww/profile-displayphoto-shrink_400_400/0/1678667807168?e=1689206400&v=beta&t=MVTyKsxWUfNWeBwE-PAS_RejhE2VmCCcJzvddqnL90s",
-            resume: "Desenvolvedora fullstack"
-        },
-        {
-            name: "Angélica Rodrigues Gomes",
-            image: "https://media.licdn.com/dms/image/D4D03AQHQNm8kW7Agww/profile-displayphoto-shrink_400_400/0/1678667807168?e=1689206400&v=beta&t=MVTyKsxWUfNWeBwE-PAS_RejhE2VmCCcJzvddqnL90s",
-            resume: "Desenvolvedora fullstack"
-        },
-        {
-            name: "Angélica Rodrigues Gomes",
-            image: "https://media.licdn.com/dms/image/D4D03AQHQNm8kW7Agww/profile-displayphoto-shrink_400_400/0/1678667807168?e=1689206400&v=beta&t=MVTyKsxWUfNWeBwE-PAS_RejhE2VmCCcJzvddqnL90s",
-            resume: "Desenvolvedora fullstack"
-        }
-    ]
     res.json(womenInTI)
 };
+
+
 
 const showHour = (req, res) => {
     const date = new Date();
@@ -35,5 +57,6 @@ const showHour = (req, res) => {
 module.exports = {
     showHello,
     women,
-    showHour
+    showHour,
+    createWoman
 }
